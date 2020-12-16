d2362b4660dc43afca2a319495325c72f53afc98
Mateus Siqueira Batista <mateus999batista@gmail.com>
2020-12-16 16:28:35 -0300

    remove html template

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  url = 'https://localhost:5001';
+
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get(this.url + '/api/users').subscribe(
+      response => {
+        this.users = response;
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+
 }
