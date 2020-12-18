69cfb33e862570623d26be3a21bd93b0cdcee637
Mateus Siqueira Batista <mateus999batista@gmail.com>
2020-12-17 22:31:54 -0300

    login controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -19,9 +19,9 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) {
             
-            if(await UserExists(registerDto.Username)) return BadRequest("Usuário já cadastrado");
+            if(await UserExists(registerDto.Username)) return BadRequest("User Alredy Exists");
             
             using var hmac = new HMACSHA512();
 
@@ -33,6 +33,22 @@ namespace API.Controllers
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) {
+            
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            if(user == null) return Unauthorized("Invalid Username");
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash =  hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length ; i++){
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Incorrect Password");
+            }
             return user;
         }
 
