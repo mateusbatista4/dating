daa7c63e3a3b7014523574bc035bbcef968ac670
Mateus Siqueira Batista <mateus999batista@gmail.com>
2020-12-18 21:56:53 -0300

    services login

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model : any = {};
+
+  token  = ''
+
+  isLogged = false
+
+  constructor(private accountService : AccountService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe(
+      response => {
+        console.log(response);
+        this.isLogged = true;
+      },
+      error => {
+        console.log(error)
+
+      }
+    )
+  }
+  logout(){
+    this.isLogged = false;
   }
 
 }
