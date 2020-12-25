5776da90c4a5a02f8d8569eea10d1f5b9a6b8edc
Mateus Siqueira Batista <mateus999batista@gmail.com>
2020-12-25 19:31:01 -0300

    primary collor comment at flutter main

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Dating app Mobile',
       theme: ThemeData(
+        //primary = #df691a
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
